57d2777f40dcdbd230774a52de469612c031c17e
dmitriano <winappdev@gmail.com>
2023-04-15 21:16:13 +0300

    cc

---
+++ Tests/ExchangeModel.h
@@ -0,0 +1,118 @@
+#include "Awl/EnumTraits.h"
+#include "Awl/Decimal.h"
+
+#include <vector>
+#include <optional>
+#include <chrono>
+
+namespace exchange
+{
+    using Decimal = awl::decimal<uint64_t, 4>;
+    
+    constexpr Decimal operator"" _d(const char* str, std::size_t len)
+    {
+        return Decimal(std::string_view(str, len));
+    }
+
+    constexpr Decimal operator"" _d(const wchar_t* str, std::size_t len)
+    {
+        return Decimal(std::wstring_view(str, len));
+    }
+
+    struct Range
+    {
+        int min;
+        int max;
+
+        AWL_STRINGIZABLE(min, max)
+    };
+
+    AWL_MEMBERWISE_EQUATABLE(Range);
+
+    struct Limits
+    {
+        Range amount;
+        Range price;
+        Range cost;
+
+        AWL_STRINGIZABLE(amount, price, cost)
+    };
+
+    AWL_MEMBERWISE_EQUATABLE(Limits);
+
+    struct Precision
+    {
+        uint8_t base;
+        uint8_t quote;
+        uint8_t amount;
+        uint8_t price;
+
+        AWL_STRINGIZABLE(base, quote, amount, price)
+    };
+
+    AWL_MEMBERWISE_EQUATABLE(Precision);
+
+    struct Market
+    {
+        Limits limits;
+        
+        std::string id;
+        
+        Precision precision;
+
+        AWL_STRINGIZABLE(limits, id, precision)
+    };
+
+    AWL_MEMBERWISE_EQUATABLE(Market)
+
+    using Clock = std::chrono::system_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+    using Duration = typename Clock::duration;
+
+    using OrderId = int64_t;
+    
+    AWL_SEQUENTIAL_ENUM(OrderSide, Buy, Sell)
+
+    AWL_SEQUENTIAL_ENUM(OrderStatus,
+        Pending,
+        Open,
+        Closed
+    )
+
+    AWL_SEQUENTIAL_ENUM(OrderType,
+        Limit,
+        Market,
+        StopLoss,
+        StopLossLimit,
+        TakeProfit,
+        TakeProfitLimit,
+        LimitMaker
+    )
+        
+    struct Order
+    {
+        std::string marketId;
+        OrderId id;
+        int64_t listId;
+        std::string clientId;
+
+        OrderSide side;
+        OrderType type;
+        OrderStatus status;
+
+        Decimal price;
+        Decimal stopPrice;
+        Decimal amount;
+        Decimal filled;
+        Decimal cost;
+        Decimal reserved;
+
+        TimePoint createTime;
+        TimePoint updateTime;
+
+        AWL_STRINGIZABLE(listId, clientId, marketId, side, type, status, id, price, stopPrice,
+            amount, filled, cost, createTime, updateTime)
+    };
+
+    AWL_MEMBERWISE_EQUATABLE(Order)
+}
